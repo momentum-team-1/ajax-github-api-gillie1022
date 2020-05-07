3d8f76a9cb7061daf1d578077a91045b3251bfe3
gillie1022 <dave.gillie@icloud.com>
2020-05-07 10:53:42 -0400

    took off the email until I can fugure out how to get in to populate

--- main.js
+++ main.js
@@ -2,20 +2,26 @@ let body = document.getElementsByTagName("body")[0];
 let header = document.createElement("header");
 header.classList.add("header");
 body.appendChild(header);
+
 let card = document.createElement("div");
 card.classList.add("card");
 body.appendChild(card);
+
 let theBasics = document.createElement("div");
 theBasics.classList.add("theBasics");
 card.appendChild(theBasics);
 theBasics.innerHTML = `<h2>The Basics</h2>`;
+
 let infoList = document.createElement("ul");
 theBasics.appendChild(infoList);
+
 let theStory = document.createElement("div");
 theStory.classList.add("theStory");
 card.appendChild(theStory);
 theStory.innerHTML = `<h2>The Story</h2><p></p>`;
-let bio = document.getElementsByTagName("p")[0]
+
+let bio = document.getElementsByTagName("p")[0];
+
 let avatar = document.createElement("div");
 avatar.classList.add("avatar");
 card.appendChild(avatar);
@@ -27,9 +33,9 @@ fetch("https://api.github.com/users/gillie1022")
     header.innerHTML = `<h1>${data.name}</h1>`;
     infoList.innerHTML = `
     <li>Name: ${data.name}</li>
-    <li>GitHub URL: <a href=${data.html_url}>gillie1022</a></li>
-    <li>email: ${data.email}</li>
+    <li>GitHub URL: <a href=${data.html_url} target="_blank">${data.login}</a></li>
     <li>Location: ${data.location}</li>`;
-    bio.textContent = `${data.bio}`
-    avatar.innerHTML = `<img src=${data.avatar_url} alt="avatar">`
-});
+    bio.textContent = `${data.bio}`;
+    avatar.innerHTML = `<img src=${data.avatar_url} alt="avatar">`;
+  });
+// <li>email: ${data.email}</li>
